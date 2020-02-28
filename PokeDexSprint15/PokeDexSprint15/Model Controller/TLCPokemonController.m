73203575a149d0f837737676c1f94297d0e1fd42
Troy Christian <apostasy8@gmail.com>
2020-02-28 13:48:14 -0500

    Add PokemonController implementation

--- PokeDexSprint15/PokeDexSprint15/Model Controller/TLCPokemonController.m
+++ PokeDexSprint15/PokeDexSprint15/Model Controller/TLCPokemonController.m
@@ -7,7 +7,37 @@
 //
 
 #import "TLCPokemonController.h"
+#import "PokeDexSprint15-Swift.h"
+
+@interface TLCPokemonController()
+
+@property (nonatomic) NSArray<TLCPokemon *> *pokemonArray; //
+
+@end
 
 @implementation TLCPokemonController
+
+-(void)retrievePokemon:(void (^)(NSError *))completion {
+    [APIController.shared retrievePokemonWithCompletion:^(NSArray<TLCPokemon *> *pokemon, NSError *error) {
+        if(error) {
+            completion(error);
+            return;
+        } else {
+            self.pokemonArray = [[NSArray alloc] initWithArray: pokemon];
+            completion(nil);
+        }
+        
+    }];
+}
+
+-(void)getPokemonDetails:(TLCPokemon *)pokemon {
+    [APIController.shared retrieveDetailsFor:pokemon];
+    
+}
+
+-(NSArray <TLCPokemon *> *)getAllPokemon {
+    return self.pokemonArray;
+}
+
 
 @end
