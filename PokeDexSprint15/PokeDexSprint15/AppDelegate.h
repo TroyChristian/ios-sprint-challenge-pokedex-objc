c38e50f1a5706e64025008da32c97fb5b5575b6a
Troy Christian <apostasy8@gmail.com>
2020-02-28 11:22:41 -0500

    initial commit

---
+++ PokeDexSprint15/PokeDexSprint15/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokeDexSprint15
+//
+//  Created by Lambda_School_Loaner_219 on 2/28/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokeDexSprint15/PokeDexSprint15/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PokeDexSprint15
+//
+//  Created by Lambda_School_Loaner_219 on 2/28/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
